20c300f3bb0f805493e4a4996d72dd4c7e663bc4
LUIZAASB <167585481+LUIZAASB@users.noreply.github.com>
2025-05-15 11:15:31 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+fuction diminuiTamanho(){
+    tamanhoSenha = tamanhoSenha-1;
+    numeroSenha.textContent = tamanhoSenha;
+}
+fuction aumentaTamanho(){
+    tamanhoSenha = tamanhoSenha+1;
+    numeroSenha.textContent = tamanhoSenha;
+
+}
+console.log(botoes);
